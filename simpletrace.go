cb9806a554f7fff133f52af80cb6fce8942a584c
Ada Cohen <ada@fieldbook.com>
2022-07-02 13:39:26 -0400

    Remove debug logging

--- simpletrace.go
+++ simpletrace.go
@@ -2,14 +2,11 @@ package simpletrace
 
 import (
 	"image"
-
-	"github.com/kr/pretty"
 )
 
 func TraceImage(img image.Image) [][]Point {
 	// Make the square map
 	squaremap := getSquaresForImage(img, DarkColorFilledFunc)
-	pretty.Println("Squares", squaremap.Inspect())
 	// Get the polygons
 	polygons := squaremap.convertSquaresToPolygons()
 	return polygons
